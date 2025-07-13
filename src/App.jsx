17e53d4b198a475fcf866abbfd3d6bf6f50f9a0d
Prikus-01 <9riyanshu.k@gmail.com>
2025-07-13 11:38:07 +0530

    setup react + tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
